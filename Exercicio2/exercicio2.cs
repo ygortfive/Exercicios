51ac7dd243a168f944d668b4ffa24198b923959e
ygortfive <ygormattos.b@gmail.com>
2017-06-27 20:35:19 -0300

     

---
+++ Exercicio2/exercicio2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//Faça um programa que exiba na tela todos os divisores de um número digitado pelo usuário.
+
+            int num = 0;
+            Console.Write("Informe um número: ");
+            num = int.Parse(Console.ReadLine());            
+            if (num == 0)
+            {
+                Console.Write("Zero não é divisor de nada, poha!");
+            }
+            else
+            {
+                Console.Write("Números divisores de " + num + " são: ");
+                for (int i = 1; i <= num; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        Console.Write(i + " ");
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercicio3/exercicio3.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//Faça um programa que dado um número digitado pelo usuário, exiba na tela se ele é ou não um número primo.
+            int num = 0;
+            var divi = new List<int>();
+            Console.Write("Informe um número: ");
+            num = int.Parse(Console.ReadLine());
+            if (num == 0)
+            {
+                Console.WriteLine("Não vou falar de novo, não é dividido por ZERO!");
+            }
+            else
+            {
+                for (int i = 1; i <= num; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        divi.Add(i);
+                    }
+                }
+
+                if (divi.Count() != 2)
+                {
+                    Console.WriteLine("Esse número não é primo!");
+                }
+                else
+                {
+                    Console.WriteLine("Esse número é primo!");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
