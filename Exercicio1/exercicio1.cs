793a10a3195cbf8099d2792aab5164307ec730d6
ygortfive <ygormattos.b@gmail.com>
2017-06-27 20:34:02 -0300

     

---
+++ Exercicio1/exercicio1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Números de 0 a 100");
+            for (int i = 0; i <= 100; i++)
+            {
+                Console.Write(i + " ");
+            }
+                Console.ReadKey();
+        }
+    }
+}
